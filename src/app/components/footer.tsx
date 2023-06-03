0cbb7e8153131bc0731b0180f6e677e9c4d61f00
KeitaTerada <keita.terada.work@gmail.com>
2023-06-03 18:10:37 +0900

    ヘッダー、フッターのコンテンツ追加

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,22 @@
 import style from './footer.module.css'
+import Link from 'next/link'
+import Image from 'next/image';
 
 export function Footer(){
     return (
-        <footer></footer>
+        <footer className={style.container}>
+            <div className={style.wrapper}>
+                <Image src="/images/dd_logo_white.png" width={120} height={78} alt=""/>
+                <h1>Dolphin Design</h1>     
+            </div>
+            <div></div>
+            <ul className={style.nav}>
+                <li><Link href="">Result</Link></li>
+                <li><Link href="">About</Link></li>
+                <li><Link href="">Service/Profile</Link></li>
+                <li><Link href="">Company</Link></li>
+            </ul>
+            <p className={style.center}>© 2023 dolphindesign</p>
+        </footer>
     )
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,21 @@
-import style from './header.module.css'
+import style from './header.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export function Header() {
     return (
-        <header>
-
+        <header className={style.wrapper}>
+            <h1 className={style.logo}>
+                <Link href="/">
+                    <Image src="/images/dd_logo.png" width={150} height={98} alt="" />
+                </Link>
+            </h1>
+            <ul className={style.nav}>
+                <li><Link href="">Result</Link></li>
+                <li><Link href="">About</Link></li>
+                <li><Link href="">Service/Profile</Link></li>
+                <li><Link href="">Company</Link></li>
+            </ul>
         </header>
     )
 }
