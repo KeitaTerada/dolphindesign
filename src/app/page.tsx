05b8bebd5db7dbb4642e67bcf50d228b692cebc1
Gakiuchi <miru.mugi0215@gmail.com>
2023-06-03 22:15:18 +0900

    lp_componentを追加しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Result } from './lp_components/result'
+import { About } from './lp_components/about'
+import { Service } from './lp_components/service'
+import { Info } from './lp_components/info'
 
 export default function Home() {
   return (  
     <main>
+      <Result />
+      <About />
+      <Service />
+      <Info />
     </main>
   )
 }
