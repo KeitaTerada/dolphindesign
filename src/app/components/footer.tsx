6010d79d3186cafcf4f3ac71934db273345eb6a3
KeitaTerada <keita.terada.work@gmail.com>
2023-06-03 22:18:29 +0900

    フッターCSSの追加をしました。

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -9,13 +9,13 @@ export function Footer(){
                 <Image src="/images/dd_logo_white.png" width={120} height={78} alt=""/>
                 <h1>Dolphin Design</h1>     
             </div>
-            <div></div>
             <ul className={style.nav}>
                 <li><Link href="">Result</Link></li>
                 <li><Link href="">About</Link></li>
                 <li><Link href="">Service/Profile</Link></li>
                 <li><Link href="">Company</Link></li>
             </ul>
+            <div className={style.border}></div>
             <p className={style.center}>© 2023 dolphindesign</p>
         </footer>
     )
